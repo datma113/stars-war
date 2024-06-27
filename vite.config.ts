0563d712f8bef00f5c5978990a3a6e19fb3f859b
= <=>
2024-06-27 18:22:05 +0700

    feature > fix build

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,12 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+//@ts-ignore
 import path from 'path';
 
 export default defineConfig({
     resolve: {
         alias: {
+            //@ts-ignore
             '@': path.resolve(__dirname, 'src'),
         },
     },
